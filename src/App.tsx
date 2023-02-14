3545b852d66d838a416fba2b9e95a44e0d3e59f4
Ana Coronel <a9nna.coronel@gmail.com>
2023-02-14 21:51:11 +0100

    Create useApiData custom hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,22 @@
+import { useEffect } from "react";
+import ToDo from "./components/ToDo/ToDo";
+import useApiData from "./hooks/useApiData";
+import { useAppSelector } from "./store/hooks";
+
 const App = () => {
-  return <div className="App"></div>;
+  const { getApiData } = useApiData();
+
+  useEffect(() => {
+    getApiData();
+  }, [getApiData]);
+
+  const toDos = useAppSelector((state) => state.tasks);
+
+  return (
+    <div className="App">
+      <ToDo toDo={toDos}></ToDo>
+    </div>
+  );
 };
 
 export default App;

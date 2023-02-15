ada9cb826b453d97c15e38f22e9d5bb81acd492a
Ana Coronel <a9nna.coronel@gmail.com>
2023-02-15 07:48:52 +0100

    Create ToDos component

--- src/components/ToDo/ToDoStyled.tsx
+++ src/components/ToDo/ToDoStyled.tsx
@@ -1,11 +1,9 @@
 import styled from "styled-components";
 
 const ToDoStyled = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 5px;
-  padding: 1rem;
+  padding: 16px;
   font-weight: 700;
+  border: 1px solid black;
   background-color: #80bdf2;
 `;
 
